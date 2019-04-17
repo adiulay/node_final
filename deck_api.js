4cad2e336e7682aa07ed059c493358f39c9f7bf1
Jimmy Truong <jtruong20@my.bcit.ca>
2019-04-17 12:41:57 -0700

    card_deck

---
+++ deck_api.js
@@ -0,0 +1,20 @@
+const axios = require('axios/index');
+const fs = require('fs');
+
+
+
+var drawCard = async (number_of_cards) => {
+    const shuffle_deck = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+    var deck_id =  shuffle_deck.data.deck_id;
+
+    const draw_card = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${number_of_cards}`);
+
+    return draw_card.data.cards
+};
+
+
+
+
+module.exports = {
+    drawCard: drawCard
+}
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ hbs.registerPartials(__dirname + '/views/partials');
 
 // const weather = require('./weather_api.js');
 const gallery = require('./gallery_api.js');
+const deck = require('./deck_api.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -20,40 +21,67 @@ app.use(bodyParser.urlencoded({
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 
+// app.get('/', (request, response) => {
+//     response.render('gallery.hbs', {
+//         title: 'Gallery',
+//         output: 'http://t0.gstatic.com/images?q=tbn:ANd9GcQ9u48pu-6IB2FnnYl_H-15le_g8Dkt5d5RN-VWiWIl_-dyJdaa'
+//     })
+// });
+
+// app.get('/gallery', async (request, response) => {
+//     response.render('gallery.hbs', {
+//         title: 'Gallery',
+//         output: ''
+//     })
+// });
+
+// app.post('/getpicture', async (request, response) => {
+//     var nasa = request.body.nasa;
+//     var gallery_list = await gallery.search(nasa);
+//
+//     if (gallery_list.length === 0 || gallery_list === 'Cannot find picture') {
+//         error_dictionary = {
+//             title: 'Gallery',
+//             output_error: 'No pictures found'
+//         };
+//         response.render('gallery.hbs', error_dictionary)
+//     } else {
+//         response.render('gallery.hbs', {
+//             title: 'Gallery',
+//             output: gallery_list[_.random(0, gallery_list.length)],
+//             output1: gallery_list[_.random(0, gallery_list.length)],
+//             output2: gallery_list[_.random(0, gallery_list.length)],
+//             output3: gallery_list[_.random(0, gallery_list.length)]
+//         })
+//     }
+// });
+
 app.get('/', (request, response) => {
-    response.render('gallery.hbs', {
-        title: 'Gallery',
-        output: 'http://t0.gstatic.com/images?q=tbn:ANd9GcQ9u48pu-6IB2FnnYl_H-15le_g8Dkt5d5RN-VWiWIl_-dyJdaa'
+    response.render('deck.hbs', {
+        title: 'DRAW YUGIOH',
+        output: 'hello'
     })
 });
 
-app.get('/gallery', async (request, response) => {
-    response.render('gallery.hbs', {
-        title: 'Gallery',
-        output: ''
-    })
-});
+app.post('/drawcard', async (request, response) => {
+    var card_drawn = request.body.card_drawn;
 
-app.post('/getpicture', async (request, response) => {
-    var nasa = request.body.nasa;
-    var gallery_list = await gallery.search(nasa);
+    var api_deck = await deck.drawCard(card_drawn);
 
-    if (gallery_list.length === 0 || gallery_list === 'Cannot find picture') {
-        error_dictionary = {
-            title: 'Gallery',
-            output_error: 'No pictures found'
-        };
-        response.render('gallery.hbs', error_dictionary)
-    } else {
-        response.render('gallery.hbs', {
-            title: 'Gallery',
-            output: gallery_list[_.random(0, gallery_list.length)],
-            output1: gallery_list[_.random(0, gallery_list.length)],
-            output2: gallery_list[_.random(0, gallery_list.length)],
-            output3: gallery_list[_.random(0, gallery_list.length)]
-        })
+    var hand = [];
+
+    var i;
+    for (i = 0; i < api_deck.length; i++) {
+        hand.push(api_deck[i].image)
     }
+
+    response.render('deck.hbs', {
+        title: 'DRAW YUGIOH',
+        output: 'Here is your output',
+        draw_card: hand
+    })
 });
+
 
 // app.post('/getweather', async (request, response) => {
 //     var city = request.body.city;
